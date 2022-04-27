38edb6b01069006cc7f77ebe503d18054997db7f
AleF2050 <99495844+AleF2050@users.noreply.github.com>
2022-04-27 11:59:04 +0200

    created Node2D pivot node for Knight.
    this allows me to specify a 'origin' point for sprites, allowing for more centered and correctly fixed animations.

--- src/objects/entities/E_Knight.gd
+++ src/objects/entities/E_Knight.gd
@@ -10,13 +10,15 @@ var _state = state.CHASE
 func _ready():
 	$Animation.play("idle")
 
+# warning-ignore:unused_argument
 func _physics_process(delta):
+	var player = get_tree().get_nodes_in_group("player").front()
+	
 	match _state:
 		state.CHASE:
-			var player_exists = get_tree().get_nodes_in_group("player")[0]
-			
 			$Animation.play("walk")
-			if player_exists:
+			if is_instance_valid(player):
 				print("There is a player.")
+				$SpritePivot.scale.x = sign(player.global_position.x - global_position.x)
 
 # https://www.youtube.com/watch?v=boHDx303FUo&list=PL9FzW-m48fn0mblTG_KFDg81AMXDPKBE5&index=11  7:30
