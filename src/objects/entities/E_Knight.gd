445b993265ed110f4fc7d6ee646c19c3ec99f9d9
AleF2050 <99495844+AleF2050@users.noreply.github.com>
2022-04-29 01:17:29 +0200

    created attack state for knight enemy

--- src/objects/entities/E_Knight.gd
+++ src/objects/entities/E_Knight.gd
@@ -3,16 +3,18 @@ extends KinematicBody2D
 # enum states
 enum state {
 	CHASE
+	ATTACK
 }
 
-var _state = state.CHASE
 export var move_speed = 30
+var _state = state.CHASE
 var speed = Vector2.ZERO
+var attack_range = 42
 
 func _ready():
 	$Animation.play("idle")
 
-# warning-ignore:unused_argument
+
 func _physics_process(delta):
 	var player = get_tree().get_nodes_in_group("player").front()
 	
@@ -22,12 +24,21 @@ func _physics_process(delta):
 			if is_instance_valid(player):
 				$SpritePivot.scale.x = sign(player.global_position.x - global_position.x)
 			
-			if get_position().distance_to(player.position) > 42:
+			if get_position().distance_to(player.position) > attack_range:
 				speed.x = ($SpritePivot.scale.x * move_speed)
 			else:
-				speed.x = 0
+				_state = state.ATTACK
 			
 			speed = move_and_slide(speed)
+		
+		state.ATTACK:
+			$Animation.play("attack")
+
+
+func _on_Animation_animation_finished(anim_name):
+	if anim_name == "attack":
+		_state = state.CHASE
+
 
 # todo:
 # - fix sprite jittering
