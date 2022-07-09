d57413c44a1e5bccced43bced6e217c53ad49973
AleF2050 <99495844+AleF2050@users.noreply.github.com>
2022-07-09 13:08:42 +0200

    substitute AnimatedSprite-related code with AnimationPlayer + simple method for swapping change state bool

--- src/objects/player/Player.gd
+++ src/objects/player/Player.gd
@@ -12,6 +12,7 @@ enum state {
 # ... INIT VARIABLES
 export var move_speed = 90
 export var roll_speed = 160
+export var can_change_state = false
 var speed = Vector2()
 var _state = state.GROUND
 
@@ -28,30 +29,37 @@ func _ready():
 
 # ... PHYSICS PROCESS (mostly for movement)
 func _physics_process(delta):
+	
+	# Disabling bool for permmitting combo input during combo animations, excluding attack animations.
+	# It checks whenever attack animations are not playing so AnimationPlayer can safely execute methods on its own.
+	if $Animation.current_animation != "n_attack1" and $Animation.current_animation != "n_attack2" and $Animation.current_animation != "n_attack3":
+		can_chain_animation(false)
+	
+	
 	# State check
 	match _state:
 		state.GROUND: # GROUND STATE
 			# Moving the character
 			if Input.is_action_pressed("k_left"):
-				$AnimatedSprite.flip_h = true
-				$AnimatedSprite.animation = "move"
+				$SpritePivot.scale.x = -1
+				$Animation.play("move")
 				$Collider.scale.x = -1
 				speed.x = -move_speed
 			elif Input.is_action_pressed("k_right"):
-				$AnimatedSprite.flip_h = false
-				$AnimatedSprite.animation = "move"
+				$SpritePivot.scale.x = 1
+				$Animation.play("move")
 				$Collider.scale.x = 1
 				speed.x = move_speed
 			else:
-				$AnimatedSprite.animation = "stand"
+				$Animation.play("stand")
 				speed = Vector2(0.0,0.0)
 			
 			# Roll character
 			if Input.is_action_just_pressed("k_action1"):
 				_state = state.ROLL
-				if $AnimatedSprite.flip_h == true:
+				if $SpritePivot.scale.x == -1:
 					speed.x = -roll_speed
-				elif $AnimatedSprite.flip_h == false:
+				elif $SpritePivot.scale.x == 1:
 					speed.x = roll_speed
 			speed = move_and_slide(speed)
 			
@@ -63,48 +71,58 @@ func _physics_process(delta):
 			
 		state.ROLL: # ROLL STATE
 			# Rolling
-			$AnimatedSprite.animation = "rolling"
+			$Animation.play("rolling")
 			if !(speed.x >= 2 or speed.x <= 2):
-				if $AnimatedSprite.flip_h == true:
+				if $SpritePivot.scale.x == -1:
 					speed.x += 2
-				elif $AnimatedSprite.flip_h == false:
+				elif $SpritePivot.scale.x == 1:
 					speed.x -= 2
 			speed = move_and_slide(speed)
 			
 			
 		state.N_ATTACK1: # NORMAL ATTACK STATE, SEQ 1
-			$AnimatedSprite.animation = "n_attack1"
-			if Input.is_action_just_pressed("k_action2") && anim_hitframe_range(2, 4):
+			$Animation.play("n_attack1")
+			if Input.is_action_just_pressed("k_action2") && can_change_state == true:
 				_state = state.N_ATTACK2
 			
 			
 		state.N_ATTACK2: # NORMAL ATTACK STATE, SEQ 2
-			$AnimatedSprite.animation = "n_attack2"
-			if Input.is_action_just_pressed("k_action2") && anim_hitframe_range(3, 4):
+			$Animation.play("n_attack2")
+			if Input.is_action_just_pressed("k_action2") && can_change_state == true:
 				_state = state.N_ATTACK3
 			
 			
 		state.N_ATTACK3: # NORMAL ATTACK STATE, SEQ 3
-			$AnimatedSprite.animation = "n_attack3"
+			$Animation.play("n_attack3")
 			
 			
-	Signals.emit_signal("state_change", _state) #Updates state for debugging
+	Signals.emit_signal("state_change", can_change_state) #Updates state for debugging
 
 
-func _on_AnimatedSprite_animation_finished():
+func _on_Animation_finished(anim_name):
 	# Stop rolling after animation or finish attack animations and move on to GROUND state
-	if $AnimatedSprite.animation == "rolling" or $AnimatedSprite.animation == "n_attack1" or $AnimatedSprite.animation == "n_attack2" or $AnimatedSprite.animation == "n_attack3":
+	if anim_name == "rolling" or anim_name == "n_attack1" or anim_name == "n_attack2" or anim_name == "n_attack3":
 		_state = state.GROUND
-		$AnimatedSprite.animation = "stand"
+		$Animation.play("stand")
 		speed.x = 0
 
 
-func anim_hitframe_range(_min, _max):
-	# Checks whenever the current animation frame is under the specified frame range before going true.
-	return $AnimatedSprite.frame >= _min and $AnimatedSprite.frame <= _max
+func can_chain_animation(enable):
+	# Method used for AnimationPlayer for enabling/disabling combo input chaining for animations
+	can_change_state = enable
+
+
+
+
+
+##SCRAPPED CODE
+#func anim_hitframe_range(_min, _max):
+#	# Checks whenever the current animation frame is under the specified frame range before going true.
+#	return $SpritePivot/AnimatedSprite.frame >= _min and $SpritePivot/AnimatedSprite.frame <= _max
 
 
 #func anim_hitframe(_frame):
 #	# Checks whenever the current animation frame is under the specified frame before going true.
-#	var _fr_range = $AnimatedSprite.speed_scale
-#	return $AnimatedSprite.frame >= _frame and $AnimatedSprite.frame < _fr_range
+#	var _fr_range = $SpritePivot/AnimatedSprite.speed_scale
+#	return $SpritePivot/AnimatedSprite.frame >= _frame and $SpritePivot/AnimatedSprite.frame < _fr_range
+
